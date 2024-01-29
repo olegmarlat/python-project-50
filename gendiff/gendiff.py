0cd728079a0430920e527ba0c28c80a5c3be08e2
Oleg Latypov <latol179@mail.ru>
2024-01-29 13:03:55 +0300

    modified gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -14,8 +14,8 @@ file_path2.json:{
   "host": "hexlet.io"
 }
 
-def generate_diff(file_path11, file_path2):
-    file1 = json.load(open(file_path11.json))
+def generate_diff(file_path1, file_path2):
+    file1 = json.load(open(file_path1.json))
     file2 = json.load(open(file_path2))
     diff = []
 
