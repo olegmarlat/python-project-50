9cacd8b4329153c66f6406ddfa9908aa12e93713
Oleg Latypov <latol179@mail.ru>
2024-02-02 11:37:10 +0300

    added makefile

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,19 +1,6 @@
 import json
 
 
-file_1.json:{
-  "host": "hexlet.io",
-  "timeout": 50,
-  "proxy": "123.234.53.22",
-  "follow": false
-}
-
-file_path2.json:{
-  "timeout": 20,
-  "verbose": true,
-  "host": "hexlet.io"
-}
-
 def generate_diff(file_path1, file_path2):
     file1 = json.load(open(file_path1.json))
     file2 = json.load(open(file_path2))
