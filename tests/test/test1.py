f67a26a0dbbda2db3759bc02c15e754485ef3cac
Oleg Latypov <latol179@mail.ru>
2024-02-25 18:24:46 +0300

    added new .yml files

--- tests/test/test1.py
+++ tests/test/test1.py
@@ -1,10 +1,16 @@
 from gendiff.gendiff import generate_diff
 
 
-file_result = open('.tests/fixtures/result_test1.txt')
+file_result = open('./tests/fixtures/result_test1.txt')
 result_test_json = file_result.read()
 
 
 def test_generate_diff():
     assert generate_diff(
         './tests/fixtures/file1.json', './tests/fixtures/file2.json') == result_test_json
+
+result_test_yml = yml.load(open('./tests/fixtures/result_test_yml.txt'))
+
+def test_generate_diff():
+    assert generate_diff(
+        file_path1: './tests/fixtures/file1.yml', file_path2: './tests/fixtures/file2.yml') == result_test_yml
