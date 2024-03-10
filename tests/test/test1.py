5e1ab4ea6c0b823523776e307f04b594de991e52
Oleg Latypov <latol179@mail.ru>
2024-03-10 20:48:32 +0300

    added new files

--- tests/test/test1.py
+++ tests/test/test1.py
@@ -9,8 +9,9 @@ def test_generate_diff():
     assert generate_diff(
         './tests/fixtures/file1.json', './tests/fixtures/file2.json') == result_test_json
 
-result_test_yml = yml.load(open('./tests/fixtures/result_test_yml.txt'))
+result_test_yml = yaml.load(open('./tests/fixtures/result_test_yml.txt'))
+
 
 def test_generate_diff():
     assert generate_diff(
-        file_path1: './tests/fixtures/file1.yml', file_path2: './tests/fixtures/file2.yml') == result_test_yml
+        './tests/fixtures/file1.yml', './tests/fixtures/file2.yml') == result_test_yml
