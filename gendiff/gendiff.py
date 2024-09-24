e87170284b32e42f8d5646e0060cc2300fb88023
Oleg Latypov <latol179@mail.ru>
2024-09-24 21:53:41 +0300

    files have been modified

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,7 +3,7 @@
 #from gendiff.formatting import format_diff
 
 
-#def generate_diff(file_1, file_2, format ='stylish'):
+def generate_diff(file_1, file_2, format ='stylish'):
 #    dict_1 = get_data(file_1)
 #    dict_2 = get_data(file_2)
 #    diff = build_diff(dict_1, dict_2)
