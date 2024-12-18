a45503764c0e99a13735c66056b18c61ae8f9dbe
Oleg Latypov <latol179@mail.ru>
2024-12-18 23:03:42 +0300

    modified:   gendiff/gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,8 +3,8 @@ from gendiff.build_diff import build_diff
 from gendiff.formatting import format_diff
 
 
-def generate_diff(file_1, file_2, format="stylish"):
-    dict_1 = get_data(file_1)
-    dict_2 = get_data(file_2)
+def generate_diff(file2_1, file2_2, format="stylish"):
+    dict_1 = get_data(file2_1)
+    dict_2 = get_data(file2_2)
     diff = build_diff(dict_1, dict_2)
     return format_diff(diff, format)
