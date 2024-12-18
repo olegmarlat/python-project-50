47ae3be152f3800e0f940b03a9729a20cce41ce8
Oleg Latypov <latol179@mail.ru>
2024-12-18 23:23:53 +0300

    modified:   gendiff/gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,10 +1,16 @@
+import os
+
 from gendiff.parser import get_data
 from gendiff.build_diff import build_diff
 from gendiff.formatting import format_diff
 
 
-def generate_diff(file2_1, file2_2, format="stylish"):
-    dict_1 = get_data(file2_1)
-    dict_2 = get_data(file2_2)
+def get_continuation(file_path):
+    return os.path.splitext(file_path)[1][1:]
+
+
+def generate_diff(file1_path, file2_path, format="stylish"):
+    dict_1 = get_data(file1_path)
+    dict_2 = get_data(file2_path)
     diff = build_diff(dict_1, dict_2)
     return format_diff(diff, format)
