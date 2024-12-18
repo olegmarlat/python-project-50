244e8837569b8208c4694d212741a2573a14d77e
Oleg Latypov <latol179@mail.ru>
2024-12-18 22:10:31 +0300

    modified:   gendiff/build_diff.py

--- gendiff/build_diff.py
+++ gendiff/build_diff.py
@@ -1,34 +1,38 @@
 def build_diff(dict1, dict2):
+    all_keys = sorted(set(dict1.keys()).union(dict2.keys()))
     diff = []
-    all_keys = set(dict1.keys()).union(dict2.keys())
 
-    for key in sorted(all_keys):
-        if key not in dict2:
-            diff.append({
-                'key': key,
-                'status': 'removed',
-                'old_value': dict1[key]
-            })
-        elif key not in dict1:
+    for key in all_keys:
+        if key not in dict1:
             diff.append({
                 'key': key,
                 'status': 'added',
-                'new_value': dict2[key]
+                'value': dict2[key]
+            })
+        elif key not in dict2:
+            diff.append({
+                'key': key,
+                'status': 'removed',
+                'new_value': dict1[key]
             })
         elif isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
             nested_diff = build_diff(dict1[key], dict2[key])
-            diff.append({
-                'key': key,
-                'status': 'nested',
-                'nested': nested_diff
-            })
+            diff.append(
+                {
+                    'key': key,
+                    'status': 'nested',
+                    'nested': nested_diff
+                }
+            )
         elif dict1[key] != dict2[key]:
-            diff.append({
-                'key': key,
-                'status': 'updated',
-                'old_value': dict1[key],
-                'new_value': dict2[key]
-            })
+            diff.append(
+                {
+                    'key': key,
+                    'status': 'updated',
+                    'old_value': dict1[key],
+                    'new_value': dict2[key]
+                }
+            )
         else:
             diff.append({
                 'key': key,
