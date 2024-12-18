303da1254971ffb060a8acff4b4d2d32e0389f90
Oleg Latypov <latol179@mail.ru>
2024-12-18 23:37:57 +0300

    modified:   tests/test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).parent
 
 
 @pytest.mark.parametrize(
-    "file_1, file_2, expected, format",
+    "file1, file2, expected, format",
     [
         ("file1_1.json," "file2_1.json", "stylish.txt", "stylish"),
         ("file1_1.yml," "file2_1.yml", "stylish.yml", "stylish"),
@@ -17,11 +17,11 @@ BASE_DIR = Path(__file__).parent
         ("file1_1.yml," "file2_1.yml", "json.txt", "json"),
     ],
 )
-def test_gendiff(file_1, file_2, expected, format):
-    file_1_path = os.path.join(BASE_DIR, "fixtures", file_1)
-    file_2_path = os.path.join(BASE_DIR, "fixtures", file_2)
+def test_gendiff(file1, file2, expected, format):
+    file1_path = os.path.join(BASE_DIR, "fixtures", file1)
+    file2_path = os.path.join(BASE_DIR, "fixtures", file2)
     expected_path = os.path.join(BASE_DIR, "fixtures", expected)
 
-    result = generate_diff(file_1_path, file_2_path, format)
+    result = generate_diff(file1_path, file2_path, format)
     with open(expected_path, "r") as expected_file:
         assert result == expected_file.read()
