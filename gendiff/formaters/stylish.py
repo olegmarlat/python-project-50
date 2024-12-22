49a3ed8ed37629dacf8e522154dd01985108a861
Oleg Latypov <latol179@mail.ru>
2024-12-22 15:08:41 +0300

    modified:   gendiff/formaters/stylish.py

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -33,15 +33,15 @@ def convert_to_stylish(diff, depth=1):
             result.append(f"{indent}{key}: {'nested'}")
         elif status == 'added':
             new_value = format_value(item['new_value'], depth)
-            result.append(f"{indent[:-2]} + {key}:{new_value}")
+            result.append(f"{indent[:-2]} + {key}: {new_value}")
         elif status == "removed":
             old_value = format_value(item['old_value'], depth)
-            result.append(f"{indent[:-2]} - {key} :{old_value}")
+            result.append(f"{indent[:-2]} - {key}: {old_value}")
         elif status == "updated":
             old_value = format_value(item["old_value"], depth)
-            new_value = format_value(item["new_value"], depth)
-            result.append(f"{indent[:-2]} - {key} :{old_value}")
-            result.append(f"{indent[:-2]} + {key} :{new_value}")
+            new_value = format_value(item['new_value'], depth)
+            result.append(f"{indent[:-2]}- {key}: {old_value}")
+            result.append(f"{indent[:-2]}+ {key}: {new_value}")
         elif status == "unchanged":
             old_value = format_value(item["old_value"], depth)
             result.append(f"{indent} {key} :{old_value}")
