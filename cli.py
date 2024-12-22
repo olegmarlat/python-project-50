b88f1874bd569238cd25a30a8901dc4681ae9dad
Oleg Latypov <latol179@mail.ru>
2024-12-22 12:49:25 +0300

    modified:   cli.py

--- cli.py
+++ cli.py
@@ -1,5 +1,7 @@
 import argparse
 
+from gendiff import generate_diff
+
 
 def parser_args():
     parser = argparse.ArgumentParser(
@@ -9,6 +11,7 @@ def parser_args():
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help="Set format of output")
     args = parser.parse_args()
-    print(args)
+    diff = generate_diff(args.first_file, args.second_file, style=args.format)
+    print(diff)
 
-    return args.first_file, args.second_file, args.format
+# return args.first_file, args.second_file, args.format
