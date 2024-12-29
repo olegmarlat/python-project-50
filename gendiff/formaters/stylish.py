c7729d2b147ecbef7c0da61b89cfbf59ceabc77b
Oleg Latypov <latol179@mail.ru>
2024-12-29 20:23:23 +0300

    modified:   gendiff/formaters/stylish.py

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -33,7 +33,7 @@ def convert_to_stylish(diff, depth=1):
             result.append(f"{indent}{key}: {nested}")
         elif status == "added":
             new_value = format_value(item["new_value"], depth)
-            result.append(f"{indent[:-2]} + {key}: {new_value}")
+            result.append(f"{indent[:-2]}+ {key}: {new_value}")
         elif status == "removed":
             old_value = format_value(item["old_value"], depth)
             result.append(f"{indent[:-2]} - {key}: {old_value}")
