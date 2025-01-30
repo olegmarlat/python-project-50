db63fe27ae0283b1d2024753639d6d29b5d728ce
Oleg Latypov <latol179@mail.ru>
2025-01-30 21:42:06 +0300

    modified tests

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,17 +6,44 @@ from gendiff import generate_diff
 BASE_DIR = Path(__file__).parent
 
 
-@pytest.mark.parametrize(
-    "file_1, file_2, expected, format",
-    [
-        ("file_1.json", "file_2.json", "stylish.txt", "stylish"),
-        ("file5.yml", "file6.yml", "stylish.txt", "stylish"),
-        ("file_1.json", "file_2.json", "plain.txt", "plain"),
-        ("file5.yml", "file6.yml", "plain.txt", "plain"),
-        ("file_1.json", "file_2.json", "json.txt", "json"),
-        ("file5.yml", "file6.yml", "json.txt", "json"),
-    ],
-)
+@pytest.mark.parametrize("file_1, file_2, expected, format", [
+    (
+        "file_1.json",
+        "file_2.json",
+        "stylish.txt",
+        "stylish"
+    ),
+    (
+        "file5.yml",
+        "file6.yml",
+        "stylish.txt",
+        "stylish"
+    ),
+    (
+        "file_1.json",
+        "file_2.json",
+        "plain.txt",
+        "plain"
+    ),
+    (
+        "file5.yml",
+        "file6.yml",
+        "plain.txt",
+        "plain"
+    ),
+    (
+        "file_1.json",
+        "file_2.json",
+        "json.txt",
+        "json"
+    ),
+    (
+        "file5.yml",
+        "file6.yml",
+        "json.txt",
+        "json"
+    )
+])
 def test_gendiff(file_1, file_2, expected, format):
     file_1_path = os.path.join(BASE_DIR, "fixtures", file_1)
     file_2_path = os.path.join(BASE_DIR, "fixtures", file_2)
