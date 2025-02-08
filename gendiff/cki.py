6eb0c74e904a2435fe7dd0048b8988ddde9046ce
Oleg Latypov <latol179@mail.ru>
2025-02-08 09:32:45 +0300

    modified:   gendiff/scripts/gendiff.py

---
+++ gendiff/cki.py
@@ -0,0 +1,19 @@
+import argparse
+
+
+def parser_args():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument(
+        '-f', '--format',
+        default='stylish',
+        help="Set format of output",
+        choices=['stylish', 'plain', 'json']
+    )
+    args = parser.parse_args()
+
+    return args.first_file, args.second_file, args.format
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from gendiff import generate_diff
-from cli import parser_args
+from gendiff.cki import parser_args
 
 
 def main():
