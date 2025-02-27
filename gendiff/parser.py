fa83f9ff26a1ff369780cac7682389734b7a323d
Oleg Latypov <latol179@mail.ru>
2025-02-27 23:15:31 +0300

    modified:   gendiff/parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -10,7 +10,7 @@ class ExtensionError(Exception):
 def parse_data(file, extension):
     if extension == '.json':
         return json.loads(file)
-    elif extension in ['yml', 'yaml']:
+    elif extension in ['.yml', '.yaml']:
         return yaml.load(file, Loader=yaml.Loader)
     else:
         raise ExtensionError(f'Unsupported file format: {extension}')
