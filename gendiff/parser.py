1a97254aeabe1b308cdf86a9b691e5f7f083b7cb
Oleg Latypov <latol179@mail.ru>
2025-03-01 21:18:44 +0300

    modified:   gendiff/parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -8,15 +8,15 @@ class ExtensionError(Exception):
 
 
 def parse_data(file, extension):
-    if extension == '.json':
+    if extension == 'json':
         return json.loads(file)
-    elif extension in ['.yml', '.yaml']:
+    elif extension in ['yml', 'yaml']:
         return yaml.load(file, Loader=yaml.Loader)
     else:
         raise ExtensionError(f'Unsupported file format: {extension}')
 
 
 def get_data(file_path):
-    extension = os.path.splitext(file_path)[1].lower()
+    extension = os.path.splitext(file_path)[1][1:].lower()
     with open(file_path) as file:
         return parse_data(file.read(), extension)
